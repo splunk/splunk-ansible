eae5d494c74f07589c6cb16fdd9e42c0826d6ceb
Hendo Lim <hendol@splunk.com>
2019-11-14 15:35:48 -0800

    redhat java, last oracle version allowing nonlogin download

--- inventory/environ.py
+++ inventory/environ.py
@@ -119,7 +119,7 @@ def getDefaultVars():
     if java_version in ['oracle:8', 'openjdk:8', 'openjdk:9','openjdk:11']:
         defaultVars["java_version"] = os.environ.get("JAVA_VERSION", "")
         if java_version == "oracle:8":
-            defaultVars["java_download_url"] = os.environ.get("JAVA_DOWNLOAD_URL", "https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz")
+            defaultVars["java_download_url"] = os.environ.get("JAVA_DOWNLOAD_URL", "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz")
             try:
                 defaultVars["java_update_version"] = re.search("jdk-8u(\d+)-linux-x64.tar.gz", defaultVars["java_download_url"]).group(1)
             except:
