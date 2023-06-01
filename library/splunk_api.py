e8eda03e3a2151018618855ed08344553de77838
Abdul K <abdulk@splunk.com>
2023-06-01 16:35:03 -0700

    save status

--- library/splunk_api.py
+++ library/splunk_api.py
@@ -3,6 +3,7 @@
 from ansible.module_utils.basic import AnsibleModule
 import os
 import requests
+# import requests_unixsocket
 import json
 
 UDS_SOCKET_PATH = "/var/run/splunk/cli.socket"
@@ -10,6 +11,7 @@ UDS_SOCKET_PATH = "/var/run/splunk/cli.socket"
 def supports_uds():
     return os.path.exists(UDS_SOCKET_PATH)
 
+# update to take svc_port variable
 def api_call_port_8089(method, endpoint, username, password, payload=None, headers=None, verify=False, status_code=None, timeout=None):
     url = f"https://127.0.0.1:8089{endpoint}"
     if headers is None:
@@ -18,20 +20,13 @@ def api_call_port_8089(method, endpoint, username, password, payload=None, heade
     auth = (username, password)
 
     session = requests.Session()
-    session.verify = verify
+    # Disable SSL verification for the session
+    session.verify = False
 
-    response = session.request(method, url, headers=headers, auth=auth, data=json.dumps(payload), timeout=timeout)
-    json_data = json.loads(response.text)
-    result = {
-        'url': url,
-        'status': json_data.status_code,
-        'headers': dict(json_data.headers),
-        'body': json_data
-    }
-
-    if status_code is not None and json_data['status_code'] not in status_code:
-        raise ValueError(f"API call failed with status code {json_data['status_code']}: {response.text}")
-    return result
+    response = session.request(method, url, headers=headers, auth=auth, data=json.dumps(payload), verify=verify, timeout=timeout)
+    if status_code is not None and response.status_code not in status_code:
+        raise ValueError(f"API call failed with status code {response.status_code}: {response.text}")
+    return response
 
 def main():
     module_args = dict(
@@ -41,7 +36,7 @@ def main():
         password=dict(type='str', required=True, no_log=True),
         body=dict(type='dict', required=False),
         headers=dict(type='dict', required=False),
-        verify=dict(type='bool', required=False, default=False),
+        verify=dict(type='bool', required=False),
         status_code=dict(type='list', required=False),
         timeout=dict(type='int', required=False)
     )
@@ -58,18 +53,26 @@ def main():
     endpoint = module.params['url']
     username = module.params['username']
     password = module.params['password']
-    payload = module.params.get('body', None)
+    payload = module.params.get('payload', None)
     headers = module.params.get('headers', None)
     verify = module.params.get('verify', False)
     status_code = module.params.get('status_code', None)
     timeout = module.params.get('timeout', None)
 
-    response = api_call_port_8089(method, endpoint, username, password, payload, headers, verify, status_code, timeout)
-
-    if response['status'] >= 200 and response['status'] < 300:
-        module.exit_json(changed=True, status=response['status'], json=response)
+    if supports_uds():
+        # TODO: Update back
+        response = api_call_port_8089(method, endpoint, username, password, payload, headers, verify, status_code, timeout)
     else:
-        module.fail_json(msg=f"API call failed with status code {response['status']}: {response['body']}")
+        response = api_call_port_8089(method, endpoint, username, password, payload, headers, verify, status_code, timeout)
+
+    if response.status_code >= 200 and response.status_code < 300:
+        try:
+            content = response.json()
+        except json.decoder.JSONDecodeError:
+            content = response.text
+        module.exit_json(changed=True, status = response.status_code ,content=content)
+    else:
+        module.fail_json(msg=f"API call failed with status code {response.status_code}: {response.text}")
 
 if __name__ == '__main__':
     main()
